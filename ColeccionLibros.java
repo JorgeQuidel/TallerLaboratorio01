f2d2d7f19bad87b7f8f767fb81c335287231aee8
Jorge Quidel <104737232+JorgeQuidel@users.noreply.github.com>
2022-08-24 09:52:07 -0400

    Actividad 2-3

--- ColeccionLibros.java
+++ ColeccionLibros.java
@@ -4,13 +4,44 @@ public class ColeccionLibros {
         String libro = "El Hobbit";
         String autor = "J.R.R. Tolkien";
         String editorial = "Ed. Planeta";
-        agregarLibro(misLibros, libro, autor, editorial);
+
         agregarLibro(misLibros, libro, autor, editorial);
         agregarLibro(misLibros, libro, autor, editorial);
 
-        System.out.println(totalLibros(misLibros));
-        System.out.println(espaciosDisponibles(misLibros));
+        buscarLibroPorNombre(misLibros, libro);
 
+        mostrarTotalLibros(misLibros);
+        mostrarEspaciosDisponibles(misLibros);
+
+        mostrarTodaColeccion(misLibros);
+
+    }
+
+    public static void mostrarEspaciosDisponibles(String[][] misLibros) {
+        System.out.println("Espacios Disponibles: " + espaciosDisponibles(misLibros));
+    }
+
+    public static void mostrarTotalLibros(String[][] misLibros) {
+        System.out.println("Total de Libros: " + totalLibros(misLibros));
+    }
+
+    public static void mostrarTodaColeccion(String[][] misLibros) {
+        System.out.println("-----COLECCION-----");
+        for (int i = 0; i < totalLibros(misLibros); i++) {
+            for (int j = 0; j < misLibros[i].length; j++) {
+                System.out.print("["+misLibros[i][j]+"]");
+            }
+            System.out.println();
+        }
+        System.out.println("-----COLECCION-----");
+    }
+
+    public static void buscarLibroPorNombre(String[][] misLibros, String libro) {
+        for (int i = 0; i < misLibros.length; i++) {
+            if(misLibros[i][0] == libro){
+                System.out.println("Libro Encontrado: " + misLibros[i][0]);
+            }
+        }
     }
 
     public static int totalLibros(String[][] misLibros) {
@@ -36,6 +67,7 @@ public class ColeccionLibros {
         misLibros[posicionDisponible][0] = libro;
         misLibros[posicionDisponible][1] = autor;
         misLibros[posicionDisponible][2] = editorial;
+
     }
 
 }
